b148b14c39825b2dcc9b5374bf7daa495a58fe8b
Kobby6-lang <koseitutu43@gmail.com>
2024-05-22 15:00:58 +0100

    ?

--- Assets/Portfolio Main/Scripts/PortfolioMain.cs
+++ Assets/Portfolio Main/Scripts/PortfolioMain.cs
@@ -2,11 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PortfolioMain : MonoBehaviour
 {
+    public Image displayImage;
+    Sprite[] imagesToDisplay;
+    int currentImageIndex;
+    bool slideShow = false;
    public void LoadMainMenu(string MainMenu) 
    {
         SceneManager.LoadScene(MainMenu);
    }
+
+    public void ShowImage(Sprite image)
+    {
+        displayImage.gameObject.SetActive (true);
+        displayImage.overrideSprite = image;
+    }
+
+    public void SlideShow(ImageHolder imageHolder) 
+    {
+        imagesToDisplay = imageHolder.images;
+        displayImage.gameObject.SetActive(true);
+        currentImageIndex = 0;
+        displayImage.overrideSprite = imagesToDisplay[currentImageIndex];
+        slideShow = true;
+    }
+    public void NextPicture() 
+    {
+        if(slideShow) 
+        {
+            if (currentImageIndex + 1 < imagesToDisplay.Length) 
+            {
+                currentImageIndex++;
+                displayImage.overrideSprite= imagesToDisplay[currentImageIndex];
+            }
+        }
+    }
 }
