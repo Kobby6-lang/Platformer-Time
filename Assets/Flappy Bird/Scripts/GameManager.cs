ee5c22cdf23e068ef94af6225e514edeba51d3bf
Kobby6-lang <koseitutu43@gmail.com>
2024-10-19 21:32:14 +0100

    More Changes.

--- Assets/Flappy Bird/Scripts/GameManager.cs
+++ Assets/Flappy Bird/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -13,6 +14,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject gameOver;
 
+    [SerializeField] private GameObject quitButton;
+
     private int score;
 
     private void Awake()
@@ -22,40 +25,48 @@ public class GameManager : MonoBehaviour
         Pause();
     }
 
-    public void Play() 
+    public void Play()
     {
         score = 0;
         scoreText.text = score.ToString();
 
-        playButton.SetActive (false);
-        gameOver.SetActive (false);
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        quitButton.SetActive(false);
+
         Time.timeScale = 1f;
         player.enabled = true;
 
         Pipes[] pipes = FindObjectsOfType<Pipes>();
 
-        for(int i = 0; i < pipes.Length; i++) 
+        for (int i = 0; i < pipes.Length; i++)
         {
             Destroy(pipes[i].gameObject);
         }
     }
 
-    public void Pause() 
+    public void Pause()
     {
         Time.timeScale = 0f;
         player.enabled = false;
     }
 
     public void GameOver()
-     {
+    {
         gameOver.SetActive(true);
         playButton.SetActive(true);
+        quitButton.SetActive(true);
 
         Pause();
-     }
-     public void IncreaseScore() 
-     {
+    }
+    public void IncreaseScore()
+    {
         score++;
         scoreText.text = score.ToString();
-     }
+    }
+
+    public void QuitGame() 
+    {
+        SceneManager.LoadSceneAsync("Portfolio");
+    }
 }
